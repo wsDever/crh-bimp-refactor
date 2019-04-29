8f4759ca15ad86bead9a6d211edf26760df6f6ff
linyupark <linyupark@gmail.com>
2019-04-29 18:53:36 +0800

    一些提示信息的文案也加入配置文件

--- src/config/jianghai.js
+++ src/config/jianghai.js
@@ -3,7 +3,15 @@
  */
 
 export default {
+  // 项目名
   appName: '江海经济圈',
+  // 版本号
+  version: '3.0',
+  // 显示文案
+  text: {
+    mobile_error: '请输入有效手机号'
+  },
+  // 接口地址
   api: {
     development: 'http://jhbackendtest.xpe.com:12271',
     production: 'http://zsbackendtest.xpe.com:28090',
